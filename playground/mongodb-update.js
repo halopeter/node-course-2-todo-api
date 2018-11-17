c3dcdfc534c0bf0a5b1d3e9e9ed5f5064cba1109
halopeter <flamesea02@gmail.com>
2018-11-17 15:56:26 +0100

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,42 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5beef1339560d21020e4877d')
+  }, {
+    $set: {
+      name: 'Peter'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5bf00ef99b64497a1f11db6b')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+//  client.close();
+});
